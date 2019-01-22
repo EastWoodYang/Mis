b4d6d7e60665deaea2582c31f36a35e52727495c
yangchengdong <yangchengdong@yingyinglicai.com>
2019-01-22 18:10:55 +0800

    optimize demo.

---
+++ micro-module/p_kotlin/src/main/kotlin/com/eastwood/demo/micromodule/kotlin/MicroModuleKotlinService.kt
@@ -0,0 +1,14 @@
+package com.eastwood.demo.micromodule.kotlin
+
+/**
+ *
+ * @author eastwood
+ * createDate: 2019-01-22
+ */
+class MicroModuleKotlinService : IMicroModuleKotlin {
+
+    override fun test() {
+
+    }
+
+}
---
+++ micro-module/p_kotlin/src/main/mis/com/eastwood/demo/micromodule/kotlin/IMicroModuleKotlin.kt
@@ -0,0 +1,12 @@
+package com.eastwood.demo.micromodule.kotlin
+
+/**
+ *
+ * @author eastwood
+ * createDate: 2019-01-22
+ */
+interface IMicroModuleKotlin {
+
+    fun test()
+
+}
